796d6e3fc771530731fa6e8da559274602e1c96c
Mikolaj5435 <mikolaj.curlej@zsmeie.edu.torun.pl>
2024-11-29 15:48:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,39 @@
 function calculate(operation) {
 //Dpkończ kod - pobierz liczby i sprawdź czy są poprawne
-        alert('Proszę wprowadzić prawidłowe liczby.');
-        return;
-    }
+    
+        a = document.getElementById("number1").value
+        b = document.getElementById("number2").value
+        if(isNaN(a) || isNaN(b)){    
+            alert('Proszę wprowadzić prawidłowe liczby.');
+        }
+        else{
+            return wynik(a,b,operation);
 
-    switch (operation) {
+        }
+    }
+function wynik(a,b,operation){
+
+    switch(operation) {
        //Dokończ funkcję działań: dodawania, odejmowania, dzielenia, mnożenia
-       
+            case operation = 1:
+                    result = parseInt(a) + parseInt(b)
+                break;
+            case operation=2:
+                    result = a - b
+                break;
+            case operation=3:
+                    result = a * b
+                break;
+            case operation=4:
+                if(b != 0){
+                    result = a / b}
+                else{
+                    alert('Nie można dzielić przez zero.');
+                    result = 'Nieznana operacja.'}
+                break;
              //uzupełnij   alert('Nie można dzielić przez zero.');
-            alert('Nieznana operacja.');
-            return;
     }
 
     document.getElementById('result').textContent = result;
+
 }
